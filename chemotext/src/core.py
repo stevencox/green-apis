8e7736ebf58e21b0d8693649acc184ee740828fb
Steven Cox <scox@renci.org>
2017-12-12 22:03:28 -0500

    chemotext

---
+++ chemotext/src/core.py
@@ -0,0 +1,13 @@
+
+class SupportControllerImpl:
+    def __init__(self):
+        x = 1
+        # init neo4j
+
+    def get_support (self, searchStringA, searchStringB):
+        return [
+            "x",
+            "z"
+        ]
+
+    
